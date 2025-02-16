f4d12d5bd25bc0caf2028f4d4f6240525562abdd
moradBoumazough <morad.boumazough@uit.ac.ma>
2025-02-16 12:36:30 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#B809C3',
+      },
+      backgroundImage: {
+        site: "url('/site-bg.jpg')",
+        about: "url('/about.png')",
+        services: "url('/services.png')",
+      },
+    },
+  },
+  plugins: [],
+};
